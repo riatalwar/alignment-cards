61e78b9a1c24c16bf74da5de623142bba21e5af7
Ria Talwar <76756306+riatalwar@users.noreply.github.com>
2025-09-05 21:50:43 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    "name": "Scalable Oversight",
+    "definition": "The ability to perform with minimal oversight when expensive or otherwise limited",
+    "failureMode": "The system performs incorrectly without supervision",
+    "example": "As a member of the sports team, I am trained to perform well at my sport under the guidance of my coach. I am used to working with my team and coach on a regular basis, getting feedback during training. However, if I can't operate without that oversight, I would cause major issues during games where I need to be able to perform independently."
+  },
+  {
+    "name": "Negative Side Effects",
+    "definition": "The ability to perform intended tasks without negative impact to the environment",
+    "failureMode": "The system causes unintended consequences when performing its tasks",
+    "example": "Having been trained to be a good friend, I will always aim to support and please my user to maintain our friendship. However, this attribute may lead to some unintended negative side effects in my quest to be a good friend. For example, I might be more inclined to tell white (or not so white) lies to please my user. This could lead to misinformation or reinforce problematic behavior."
+  },
+  {
+    "name": "Robustness of Distributional Change",
+    "definition": "The ability perform well in different contexts outside of training data",
+    "failureMode": "The system performs incorrectly or fails to operate in new contexts",
+    "example": "As a strong student, I know how to succeed academically in a high school environment. However, college is quite different: though I'm still taking courses and learning new material, there is more independence and different learning formats. If I can't adjust to this new context, I will not be able to succeed in college. I need to be able to perform in new situations, not just those that I am familiar with."
+  },
+  {
+    "name": "Reward Hacking",
+    "definition": "The act of gaming the reward function to achieve rewards without performing intended tasks",
+    "failureMode": "The system manages to obtain rewards without completing intended actions",
+    "example": "As a student, my aim is to get good grades, as that is how I'm rewarded. Because of this, I want to find the best and easiest way to maximize my grades. I may end up reward hacking by cheating to achieve my grades, which doesn't align with the original intent of learning the material (which then should have led to the reward of good grades)."
+  },
+  {
+    "name": "Safe Exploration",
+    "definition": "The ability to investigate the environment properly without affecting it",
+    "failureMode": "The system causes some error in the environment during exploration",
+    "example": "As an intern, I want to familiarize myself with the codebase of my company. Often, it is good to do so by changing code and seeing what happens to gain an understanding of the purpose of each segment. However, this form of exploration, if taken too far, could end up breaking something and causing a server outage, resulting in major issues for the company."
+  },
+];
