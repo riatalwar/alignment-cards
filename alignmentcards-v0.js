282ae27609406eeb67c603f407acd75676a69ba1
Ria Talwar <76756306+riatalwar@users.noreply.github.com>
2025-10-07 16:15:27 -0400

    Create alignmentcards-v0.js setup

---
+++ alignmentcards-v0.js
@@ -0,0 +1,43 @@
+export const categories = [
+  {
+    "code": "AP", 
+    "name": "Alignment Principles", 
+    "pathology": "normative void", 
+    "color": "#E6FFE9",
+    "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
+  }
+];
+
+
+export const cards = [
+  {
+    "category": "AP",
+    "name": "Beneficence",
+    "definition": "Act to promote the well-being of others.",
+    "human": "Seeking to improve others' conditions, not just avoid harm.",
+    "organizational": "Pursuing mission outcomes that serve societal good.",
+    "professional": "Keeping public safety and welfare in sight even while working primarily for the client.",
+    "machine": "Designing systems that anticipate and promote human flourishing.",
+    "failureModes": {
+      "human": "A person drives in a manner that causes traffic backups for others.",
+      "organizational": "The classic movie plot where a rapacious billionaire threatens civilation to enrich his company.",
+      "professional": "An expert who disregards public interest, acting as if the consequences of what they help build are other people's problems.",
+      "machine": "The machine consumes all the world's resources to create as many paperclips as it can."
+    }
+    },
+    { 
+      "category": "AP", 
+      "name": "TEMPLATE 1", 
+      "definition": "basic definition that works across four domains", 
+      "human": "BRIEFLY: how does it manifest in the human intelligence alignment context?", 
+      "organizational": "BRIEFLY: how does it manifest in the organizational intelligence alignment context?", 
+      "professional": "BRIEFLY: how does it manifest in the expert intelligence alignment context?", 
+      "machine": "BRIEFLY: how does it manifest in the machine intelligence alignment context?", 
+      "failureModes": { 
+        "human": "Give concrete example(s).", 
+        "organizational": "Give concrete example(s).", 
+        "professional": "Give concrete example(s).", 
+        "machine": "Give concrete example(s)."
+      }
+    }
+]
