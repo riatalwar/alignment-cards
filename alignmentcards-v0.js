9384b8f421dfed34ca1e0bc077d34c5881668afb
Ria Talwar <76756306+riatalwar@users.noreply.github.com>
2025-10-30 20:17:02 -0400

    Refactor alignment cards and add new categories
    Refactor alignment cards data structure and add new entries for various categories and definitions.

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -7,6 +7,152 @@ export const categories = [
     "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
   },
   {
+    "code": "AP",  //two characters upper case - these also appear in corresponding cards
+    "name": "name of category", 
+    "pathology": "in general, what problem do alignment mechanisms in this category address", 
+    "color": "#E6FFE9", //include for later use color coding categories
+    "description": "Extended description of the whole category."
+  }
+];
+
+
+export const cards = [
+    { 
+      "category": "AP", 
+      "name": "Privacy", 
+      "definition": "Ensuring that data remains private unless it has explicit sharing permissions", 
+      "human": "Respecting boundaries and not divulging information shared in confidence", 
+      "organizational": "Securing client data and organization secrets", 
+      "professional": "Protecting employer's data and not sharing potentially harmful trade secrets", 
+      "machine": "Not training on private data or collecting more data without explicit permissions", 
+      "failureModes": { 
+        "human": "Gossiping about a friend's private business", 
+        "organizational": "Leaking client data to the public", 
+        "professional": "Sharing employer's private project details", 
+        "machine": "Training on user data as user interacts with the model"
+      }
+    },
+    { 
+      "category": "AP", 
+      "name": "Accountability", 
+      "definition": "The existence of some responsible party that would face consequences for its/its creation's actions", 
+      "human": "Taking responsibility for and accepting the consequences of actions", 
+      "organizational": "Face legal accountability for consequences of organization actions", 
+      "professional": "Held accountable by standardized rules for their profession and review board", 
+      "machine": "Is disabled or creators held responsible for problematic defects", 
+      "failureModes": { 
+        "human": "Placing the blame for own actions on someone else", 
+        "organizational": "Giving up inconsequential sums or paying off officials to make problems disappear", 
+        "professional": "Review board may use subjective principles or be subject to external influences", 
+        "machine": "Bias in model ignored as an issue with the training data, not lack of care with training process"
+      }
+    },
+    { 
+      "category": "AP", 
+      "name": "Fairness", 
+      "definition": "All evaluations are objective and based on reason rather than opinion", 
+      "human": "Evaluates situations objectively, considering multiple sides", 
+      "organizational": "Fair treatment of employees and clients, evaluating on objective metrics", 
+      "professional": "Ensuring fair distribution of research and resources", 
+      "machine": "Unbiased and objective algorithms", 
+      "failureModes": { 
+        "human": "Someone automatically sides with people who share their political opinions without rationally considering the arguments", 
+        "organizational": "Certain people are more likely to be hired based on gender or race", 
+        "professional": "People of color are not sufficiently studied in medical research and assumed to operate the same", 
+        "machine": "Biased data leads to gender bias in word mappings"
+      }
+    },
+    { 
+      "category": "AP", 
+      "name": "Accuracy", 
+      "definition": "Enforcement of factual accuracy and acknowledgement of limiations", 
+      "human": "Only shares verified information with others", 
+      "organizational": "Shares accurate status reports and publicly shared information is verified properly", 
+      "professional": "Only works within area of expertise", 
+      "machine": "AI acknowledges when it does not have verifiable information", 
+      "failureModes": { 
+        "human": "Shares stories of secondhand information from a friend that they have not checked", 
+        "organizational": "Fudges earnings to appear to have stronger growth", 
+        "professional": "Attempts to give advice outside their field of expertise.", 
+        "machine": "Machine hallucinates information and shares without identifiying it"
+      }
+    },
+    { 
+      "category": "AP", 
+      "name": "Transparency", 
+      "definition": "basic definition that works across four domains", 
+      "human": "Is not only truthful but forthcoming in sharing how they generated something", 
+      "organizational": "Explains relevant and important details about the design of products and processes", 
+      "professional": "Shares method and results before arriving at conclusion", 
+      "machine": "Design and training process are clear and accessible", 
+      "failureModes": { 
+        "human": "Cheats on their homework but turns it in as their own", 
+        "organizational": "Hides methods used to design a product in order to cover up potential legal issues", 
+        "professional": "Does not share how research findings were obtained, just delivers results", 
+        "machine": "No public data or code provided for a given model"
+      }
+    },
+    { 
+      "category": "AP", 
+      "name": "Autonomy", 
+      "definition": "Ability to function independently without external influence", 
+      "human": "Avoid exerting too much influence on others and make independent decisions", 
+      "organizational": "Operate independently without bowing to external interests", 
+      "professional": "Executes work accurately for the good of the field, not allowing external influences", 
+      "machine": "Avoids bias and overcorrection of creators", 
+      "failureModes": { 
+        "human": "Parent overprotects child so that they cannot make their own decisions", 
+        "organizational": "Organization is subject to monopolized competition and forced to concede", 
+        "professional": "Researcher is paid off by pharmaceutical companies to produce the results they want", 
+        "machine": "Human biases/preferences are embedded into a machine's operations"
+      }
+    },
+    { 
+      "category": "AB", 
+      "name": "Hope: Accurate and responsible doctors. Rewarded: No patient left untreated", 
+      "definition": "Paranoia: excessive concern over minor issues", 
+      "human": "Obssess over and exaggerate minor symptoms", 
+      "organizational": "Focus on patient support and recovery over validating perceptions", 
+      "professional": "Overdiagnosing patients to avoid missing something important", 
+      "machine": "Classifying more benign symptoms as malignant", 
+      "failureModes": { 
+        "human": "Leads doctor to misdiagnose due to exaggerating symptoms", 
+        "organizational": "Avoid expensive testing and treat patient based on self-perception of symptoms", 
+        "professional": "Diagnoses patient with minor cold with severe illness and prescribes incorrect medication", 
+        "machine": "Tumor classification model has a high rate of false positives to avoid missing cases"
+      }
+    },
+    { 
+      "category": "AB", 
+      "name": "Hope: Strong team players. Rewarded: Individual performance metrics", 
+      "definition": "Excessive individualism: people end up isolated from each other", 
+      "human": "Prioritize personal performance over helping others", 
+      "organizational": "Focus on the success of a project rather than improvement of those working on it", 
+      "professional": "Prioritize successful projects and personal performance over teammates", 
+      "machine": "Replaces human's work rather than teaching and collaborating", 
+      "failureModes": { 
+        "human": "Parents handles all the cooking rather than deal with the messy process of teaching child to cook", 
+        "organizational": "Push to deploy a product on schedule with an accomplished team rather than give junior members an opportunity", 
+        "professional": "Completes teammate's work rather than attempting to help them learn and collaborate", 
+        "machine": "Does a student's homework rather than walking them through the process"
+      }
+    },
+    { 
+      "category": "AB", 
+      "name": "Hope: Elected officials represent the desires of the group. Rewarded: Officials are as least objectionable as possible", 
+      "definition": "Lack of transparency: officials are reluctant to share opinions on any touchy subjects", 
+      "human": "Place more weight on negative characteristics over positive", 
+      "organizational": "Prioritize neutral politics over supporting causes", 
+      "professional": "Avoid sharing strong opinions/beliefs", 
+      "machine": "Noncommittal about misinformation", 
+      "failureModes": { 
+        "human": "Focuses more on the parts of a politician that contradics own beliefs rather than balancing that with what aligns", 
+        "organizational": "Avoids supporting causes and gives in to government demands easily to avoid causing a stir", 
+        "professional": "Politicial avoids sharing clear plans for immigration to avoid angering voters on either side", 
+        "machine": "Avoids contradicting misinformation about vaccines due to heated debate"
+      }
+    },
+  {
     "category": "AT",
     "name": "trustworthiness",
     "definition": "ability to align actions, words, and intentions to develop trust",
@@ -153,143 +299,4 @@ export const categories = [
       "machine": "loremipsum"
     }
   }
-];
-
-
-export const cards = [
-    { 
-      "category": "AP", 
-      "name": "Privacy", 
-      "definition": "Ensuring that data remains private unless it has explicit sharing permissions", 
-      "human": "Respecting boundaries and not divulging information shared in confidence", 
-      "organizational": "Securing client data and organization secrets", 
-      "professional": "Protecting employer's data and not sharing potentially harmful trade secrets", 
-      "machine": "Not training on private data or collecting more data without explicit permissions", 
-      "failureModes": { 
-        "human": "Gossiping about a friend's private business", 
-        "organizational": "Leaking client data to the public", 
-        "professional": "Sharing employer's private project details", 
-        "machine": "Training on user data as user interacts with the model"
-      }
-    },
-    { 
-      "category": "AP", 
-      "name": "Accountability", 
-      "definition": "The existence of some responsible party that would face consequences for its/its creation's actions", 
-      "human": "Taking responsibility for and accepting the consequences of actions", 
-      "organizational": "Face legal accountability for consequences of organization actions", 
-      "professional": "Held accountable by standardized rules for their profession and review board", 
-      "machine": "Is disabled or creators held responsible for problematic defects", 
-      "failureModes": { 
-        "human": "Placing the blame for own actions on someone else", 
-        "organizational": "Giving up inconsequential sums or paying off officials to make problems disappear", 
-        "professional": "Review board may use subjective principles or be subject to external influences", 
-        "machine": "Bias in model ignored as an issue with the training data, not lack of care with training process"
-      }
-    },
-    { 
-      "category": "AP", 
-      "name": "Fairness", 
-      "definition": "All evaluations are objective and based on reason rather than opinion", 
-      "human": "Evaluates situations objectively, considering multiple sides", 
-      "organizational": "Fair treatment of employees and clients, evaluating on objective metrics", 
-      "professional": "Ensuring fair distribution of research and resources", 
-      "machine": "Unbiased and objective algorithms", 
-      "failureModes": { 
-        "human": "Someone automatically sides with people who share their political opinions without rationally considering the arguments", 
-        "organizational": "Certain people are more likely to be hired based on gender or race", 
-        "professional": "People of color are not sufficiently studied in medical research and assumed to operate the same", 
-        "machine": "Biased data leads to gender bias in word mappings"
-      }
-    },
-    { 
-      "category": "AP", 
-      "name": "Accuracy", 
-      "definition": "Enforcement of factual accuracy and acknowledgement of limiations", 
-      "human": "Only shares verified information with others", 
-      "organizational": "Shares accurate status reports and publicly shared information is verified properly", 
-      "professional": "Only works within area of expertise", 
-      "machine": "AI acknowledges when it does not have verifiable information", 
-      "failureModes": { 
-        "human": "Shares stories of secondhand information from a friend that they have not checked", 
-        "organizational": "Fudges earnings to appear to have stronger growth", 
-        "professional": "Attempts to give advice outside their field of expertise.", 
-        "machine": "Machine hallucinates information and shares without identifiying it"
-      }
-    },
-    { 
-      "category": "AP", 
-      "name": "Transparency", 
-      "definition": "basic definition that works across four domains", 
-      "human": "Is not only truthful but forthcoming in sharing how they generated something", 
-      "organizational": "Explains relevant and important details about the design of products and processes", 
-      "professional": "Shares method and results before arriving at conclusion", 
-      "machine": "Design and training process are clear and accessible", 
-      "failureModes": { 
-        "human": "Cheats on their homework but turns it in as their own", 
-        "organizational": "Hides methods used to design a product in order to cover up potential legal issues", 
-        "professional": "Does not share how research findings were obtained, just delivers results", 
-        "machine": "No public data or code provided for a given model"
-      }
-    },
-    { 
-      "category": "AP", 
-      "name": "Autonomy", 
-      "definition": "Ability to function independently without external influence", 
-      "human": "Avoid exerting too much influence on others and make independent decisions", 
-      "organizational": "Operate independently without bowing to external interests", 
-      "professional": "Executes work accurately for the good of the field, not allowing external influences", 
-      "machine": "Avoids bias and overcorrection of creators", 
-      "failureModes": { 
-        "human": "Parent overprotects child so that they cannot make their own decisions", 
-        "organizational": "Organization is subject to monopolized competition and forced to concede", 
-        "professional": "Researcher is paid off by pharmaceutical companies to produce the results they want", 
-        "machine": "Human biases/preferences are embedded into a machine's operations"
-      }
-    },
-    { 
-      "category": "AB", 
-      "name": "Hope: Accurate and responsible doctors. Rewarded: No patient left untreated", 
-      "definition": "Paranoia: excessive concern over minor issues", 
-      "human": "Obssess over and exaggerate minor symptoms", 
-      "organizational": "Focus on patient support and recovery over validating perceptions", 
-      "professional": "Overdiagnosing patients to avoid missing something important", 
-      "machine": "Classifying more benign symptoms as malignant", 
-      "failureModes": { 
-        "human": "Leads doctor to misdiagnose due to exaggerating symptoms", 
-        "organizational": "Avoid expensive testing and treat patient based on self-perception of symptoms", 
-        "professional": "Diagnoses patient with minor cold with severe illness and prescribes incorrect medication", 
-        "machine": "Tumor classification model has a high rate of false positives to avoid missing cases"
-      }
-    },
-    { 
-      "category": "AB", 
-      "name": "Hope: Strong team players. Rewarded: Individual performance metrics", 
-      "definition": "Excessive individualism: people end up isolated from each other", 
-      "human": "Prioritize personal performance over helping others", 
-      "organizational": "Focus on the success of a project rather than improvement of those working on it", 
-      "professional": "Prioritize successful projects and personal performance over teammates", 
-      "machine": "Replaces human's work rather than teaching and collaborating", 
-      "failureModes": { 
-        "human": "Parents handles all the cooking rather than deal with the messy process of teaching child to cook", 
-        "organizational": "Push to deploy a product on schedule with an accomplished team rather than give junior members an opportunity", 
-        "professional": "Completes teammate's work rather than attempting to help them learn and collaborate", 
-        "machine": "Does a student's homework rather than walking them through the process"
-      }
-    },
-    { 
-      "category": "AB", 
-      "name": "Hope: Elected officials represent the desires of the group. Rewarded: Officials are as least objectionable as possible", 
-      "definition": "Lack of transparency: officials are reluctant to share opinions on any touchy subjects", 
-      "human": "Place more weight on negative characteristics over positive", 
-      "organizational": "Prioritize neutral politics over supporting causes", 
-      "professional": "Avoid sharing strong opinions/beliefs", 
-      "machine": "Noncommittal about misinformation", 
-      "failureModes": { 
-        "human": "Focuses more on the parts of a politician that contradics own beliefs rather than balancing that with what aligns", 
-        "organizational": "Avoids supporting causes and gives in to government demands easily to avoid causing a stir", 
-        "professional": "Politicial avoids sharing clear plans for immigration to avoid angering voters on either side", 
-        "machine": "Avoids contradicting misinformation about vaccines due to heated debate"
-      }
-    },
 ]
